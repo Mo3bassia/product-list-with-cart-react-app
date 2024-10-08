d45482716991c304689647ca14e7a4a72c43441d
Mohamed Abassia <mo3bassia@gmail.com>
2024-10-08 09:19:56 +0300

    finish static part of desserts component and fetch data from json file

--- src/App.js
+++ src/App.js
@@ -1,5 +1,55 @@
+import { useEffect, useState } from "react";
+
 function App() {
-  return <h1>Product List</h1>;
+  return (
+    <div className="container">
+      <Desserts />
+    </div>
+  );
+}
+
+function Desserts() {
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    fetch("./data.json")
+      .then((response) => response.json())
+      .then((data) => {
+        setData(data);
+      });
+  }, []);
+
+  return (
+    <div className="desserts">
+      <h1 className="heading-title">Desserts</h1>
+      <div className="deserts_container">
+        {data.map((el) => (
+          <DessertBox key={el.name} el={el} />
+        ))}
+      </div>
+    </div>
+  );
+}
+
+function DessertBox({ el }) {
+  return (
+    <div className="desert-box">
+      <div className="desert_image">
+        <img src={`${el.image.desktop}`} alt={el.name}></img>
+      </div>
+      <Button className="btn-empty">
+        <i className="fa-solid fa-cart-shopping"></i> <span> Add To Cart</span>
+      </Button>
+      <div className="desert_description">
+        <p className="description_category">{el.category}</p>
+        <h3 className="description_name">{el.name}</h3>
+        <p className="description_price">${el.price.toFixed(2)}</p>
+      </div>
+    </div>
+  );
+}
+
+function Button({ children, className }) {
+  return <button className={className}>{children}</button>;
 }
 
 export default App;
